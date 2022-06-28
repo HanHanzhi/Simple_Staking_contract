0e31c3bb1a371b392ccc994f238314200d8ea41d
Hanzhi <Hanzhi100@outlook.com>
2022-06-28 17:52:15 +0800

    add additional test case

--- test/staking.test.js
+++ test/staking.test.js
@@ -2,6 +2,10 @@ const chai = require("chai");
 const { ethers, upgrades, deployments } = require("hardhat");
 const { expect } = require("chai");
 const { inputToConfig } = require("@ethereum-waffle/compiler");
+const { moveBlocks } = require("../utils/move-blocks");
+const { moveTime } = require("../utils/move-time");
+
+const SECONDS_IN_A_DAY = 86400;
 
 describe("Staking test", function () {
   let staking;
@@ -25,5 +29,11 @@ describe("Staking test", function () {
     await staking.stake(stakeAmount);
     const startingEarned = await staking.earned(deployer.address);
     console.log("Earned: %d", startingEarned);
+
+    //movetime
+    await moveTime(SECONDS_IN_A_DAY);
+    await moveBlocks(1);
+    const endingEarned = await staking.earned(deployer.address);
+    console.log("Earned: %d after 1 day and 1 block", endingEarned);
   });
 });
--- utils/move-blocks.js
+++ utils/move-blocks.js
@@ -3,7 +3,7 @@ const { network } = require("hardhat");
 async function moveBlocks(amount) {
   console.log("Moving blocks...");
   for (let index = 0; index < amount; index++) {
-    await network.provider.requires({
+    await network.provider.request({
       method: "evm_mine",
       params: [],
     });
